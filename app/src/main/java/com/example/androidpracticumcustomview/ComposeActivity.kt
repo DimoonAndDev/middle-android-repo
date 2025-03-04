a4076be3eb426ae37faed4fdb48325bc94a454b1
Dmitriy Kuznetsov <dimoonoomid@yandex.ru>
2025-03-05 00:15:58 +0300

    Добавлены анимации Compose экрана

--- app/src/main/java/com/example/androidpracticumcustomview/ComposeActivity.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/ComposeActivity.kt
@@ -3,7 +3,6 @@ package com.example.androidpracticumcustomview
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -16,30 +15,24 @@ import com.example.androidpracticumcustomview.ui.theme.CustomContainerCompose
 class ComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             AndroidPracticumCustomViewTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
                     CustomContainerCompose(
-                        firstChild = { FirstChild() },
-                        secondChild = { SecondChild() })
+                        firstChild = {
+                            Text(
+                                text = "first_child",
+                                color = Color.Black
+                            )
+                        },
+                        secondChild = {
+                            Text(
+                                text = "second_child",
+                                color = Color.Black
+                            )
+                        })
                 }
             }
         }
     }
-}
-
-@Composable
-fun FirstChild() {
-    Text(
-        text = "first_child"
-    )
-}
-
-@Composable
-fun SecondChild() {
-    Text(
-        text = "second_child",
-        color = Color.Black
-    )
 }
--- app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainerCompose.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainerCompose.kt
@@ -1,11 +1,13 @@
 package com.example.androidpracticumcustomview.ui.theme
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -14,6 +16,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.window.layout.WindowMetricsCalculator
 import kotlinx.coroutines.delay
 
 /*
@@ -31,46 +36,71 @@ fun CustomContainerCompose(
 ) {
     // Блок создания и инициализации переменных
     // ..
-    var view1Visible by remember { mutableStateOf(false) }
-    var view2Visible by remember { mutableStateOf(false) }
+    var firstChildVisible by remember { mutableStateOf(false) }
+    var secondChildVisible by remember { mutableStateOf(false) }
+    var firstChildMoved by remember { mutableStateOf(false) }
+    var secondChildMoved by remember { mutableStateOf(false) }
 
     // Блок активации анимации при первом запуске
     LaunchedEffect(Unit) {
-        view1Visible = true
+        firstChildVisible = true
+        firstChildMoved = true
         delay(2000)
-        view2Visible = true
+        secondChildVisible = true
+        secondChildMoved = true
     }
+    val windowMetrics = WindowMetricsCalculator.getOrCreate().computeCurrentWindowMetrics(
+        LocalContext.current
+    )
+    val bounds = windowMetrics.bounds
+    var result = 0
+    val resourceId = LocalContext.current.resources.getIdentifier("status_bar_height", "dimen", "android")
+    if (resourceId > 0) {
+        result = LocalContext.current.resources.getDimensionPixelSize(resourceId)
+    }
+    val density = LocalContext.current.resources.displayMetrics.density
+    val childWayY = (bounds.height() / 2 - result - result/2)/density
+    val offsetFirstChildY by animateDpAsState(
+        targetValue = if (firstChildMoved) (-childWayY).dp else 0.dp,
+        animationSpec = tween(durationMillis = 5000)
+    )
+    val offsetSecondChildY by animateDpAsState(
+        targetValue = if (secondChildMoved) (childWayY).dp else 0.dp,
+        animationSpec = tween(durationMillis = 5000)
+    )
 
-    // Основной контейнер
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Box(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier.wrapContentSize(),
+            contentAlignment = Alignment.Center
+        ) {
             AnimatedVisibility(
-                visible = view1Visible,
-                modifier = Modifier,
+                visible = firstChildVisible,
                 enter = fadeIn(
                     animationSpec = tween(2000)
                 ),
-                exit = fadeOut(
-                    animationSpec = tween(300)
-                )
+                modifier = Modifier.offset(y = offsetFirstChildY)
             ) {
-                firstChild()            }
+                firstChild()
+            }
         }
-        Box(modifier = Modifier.fillMaxSize()) {
+
+        Box(
+            modifier = Modifier.wrapContentSize(),
+            contentAlignment = Alignment.Center
+        ) {
             AnimatedVisibility(
-                visible = view2Visible,
-                modifier = Modifier,
+                visible = secondChildVisible,
                 enter = fadeIn(
-                    animationSpec = tween(5000)
+                    animationSpec = tween(2000)
                 ),
-                exit = fadeOut(
-                    animationSpec = tween(300)
-                )
+                modifier = Modifier.offset(y = offsetSecondChildY)
             ) {
-                secondChild()            }
+                secondChild()
+            }
         }
 
     }
